0f271f9b4466400991d4f63a14ac5ff6114e0661
Kivlyuk Yura <kivlyukyura@gmail.com>
2023-10-26 03:46:31 +0300

    Updates

--- static/js/888.befc4b69.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkbackendless_task=self.webpackChunkbackendless_task||[]).push([[888],{888:function(s,e,a){a.r(e);a(791);var t=a(184);e.default=function(){return(0,t.jsx)("div",{children:(0,t.jsx)("img",{alt:"chart",src:"../assets/chart.png"})})}}}]);
-//# sourceMappingURL=888.befc4b69.chunk.js.map
